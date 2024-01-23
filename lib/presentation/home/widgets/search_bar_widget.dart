277990ac330cacbc880ab7acc9953318146b2b67
Win Ko <40586785+WinKoKo91@users.noreply.github.com>
2024-01-24 00:35:10 +0630

    fix: search city name update ui

--- lib/presentation/home/widgets/search_bar_widget.dart
+++ lib/presentation/home/widgets/search_bar_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sunshine/presentation/home/bloc/air_pollution_bloc.dart';
 import 'package:sunshine/presentation/home/bloc/air_pollution_event.dart';
+import 'package:sunshine/presentation/home/bloc/home_state.dart';
 
 import '../../../core/utils/debouncer.dart';
 import '../../../domain/entities/location.dart';
@@ -16,7 +17,6 @@ import '../bloc/location_state.dart';
 class SearchBarWidget extends StatefulWidget {
   const SearchBarWidget({
     super.key,
-
   });
 
   @override
@@ -30,6 +30,7 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
   @override
   void initState() {
     searchController.addListener(searchListener);
+
     super.initState();
   }
 
@@ -42,101 +43,108 @@ class _SearchBarWidgetState extends State<SearchBarWidget> {
 
   @override
   Widget build(BuildContext context) {
-
-
-    return SearchAnchor(
-      searchController: searchController,
-      builder: (BuildContext context, SearchController controller) {
-        return BlocConsumer<LocationBloc, LocationState>(
-          listener: (context, state) {
-            if (state is CurrentLocationLoaded) {
-              onGetCurrentLocation(state.lat, state.lon);
-            }
-          },
-          builder: (context, state) {
-            return SearchBar(
-              key: const Key('search_bar_widget'),
-              hintText: 'Search city...',
-              controller: controller,
-              padding: const MaterialStatePropertyAll<EdgeInsets>(
-                  EdgeInsets.only(left: 16.0, right: 8.0)),
-              leading: const Icon(Icons.search),
-              onTap: controller.openView,
-              onChanged: (_) {
-                controller.openView();
-              },
-              onSubmitted: (value) {
-                if (value.isNotEmpty) {
-                  context.read<LocationBloc>().add(OnCitySubmit(value));
-                }
-              },
-              trailing: [
-                state is CurrentLocationLoading
-                    ? Container(
-                        height: 24,
-                        width: 24,
-                        margin: const EdgeInsets.only(right: 8),
-                        child: const CircularProgressIndicator())
-                    : IconButton(
-                        key: const Key('icon_button_location'),
-                        onPressed: () {
-                          searchController.clear();
-                          context
-                              .read<LocationBloc>()
-                              .add(GetCurrentLocation());
-                        },
-                        icon: const Icon(
-                          Icons.my_location,
-                          color: Colors.white,
-                        ),
-                      )
-              ],
-            );
-          },
-        );
-      },
-      suggestionsBuilder: (BuildContext context, SearchController controller) {
-        return [
-          BlocBuilder<LocationBloc, LocationState>(builder: (context, state) {
-            if (state is LocationSearchSuccessState) {
-              var locations = state.locations;
-              return locations.isEmpty
-                  ? const SizedBox(
-                      height: 200, child: Icon(Icons.hourglass_empty))
-                  : ListView.builder(
-                      key: const Key('location_data'),
-                      shrinkWrap: true,
-                      itemCount: locations.length,
-                      itemBuilder: (context, index) {
-                        final LocationEntity location = locations[index];
-                        return ListTile(
-                          leading: const Icon(Icons.location_pin),
-                          title: Text(location.name!),
-                          subtitle:
-                              Text("${location.state}, ${location.country}"),
-                          onTap: () {
-                            controller.closeView(location.name!);
-                            FocusScope.of(context).unfocus();
-                            onGetCurrentLocation(location.lat!, location.lon!);
+    return BlocBuilder<HomeBloc, HomeState>(builder: (context, state) {
+      if (state is WeatherLoaded) {
+        if (searchController.text.isEmpty) {
+          searchController.text = state.data.cityName;
+        }
+      }
+      return SearchAnchor(
+        searchController: searchController,
+        builder: (BuildContext context, SearchController controller) {
+          return BlocConsumer<LocationBloc, LocationState>(
+            listener: (context, state) {
+              if (state is CurrentLocationLoaded) {
+                onGetCurrentLocation(state.lat, state.lon);
+              }
+            },
+            builder: (context, state) {
+              return SearchBar(
+                key: const Key('search_bar_widget'),
+                hintText: 'Search city...',
+                controller: controller,
+                padding: const MaterialStatePropertyAll<EdgeInsets>(
+                    EdgeInsets.only(left: 16.0, right: 8.0)),
+                leading: const Icon(Icons.search),
+                onTap: controller.openView,
+                onChanged: (_) {
+                  controller.openView();
+                },
+                onSubmitted: (value) {
+                  if (value.isNotEmpty) {
+                    context.read<LocationBloc>().add(OnCitySubmit(value));
+                  }
+                },
+                trailing: [
+                  state is CurrentLocationLoading
+                      ? Container(
+                          height: 24,
+                          width: 24,
+                          margin: const EdgeInsets.only(right: 8),
+                          child: const CircularProgressIndicator())
+                      : IconButton(
+                          key: const Key('icon_button_location'),
+                          onPressed: () {
+                            searchController.clear();
+                            context
+                                .read<LocationBloc>()
+                                .add(GetCurrentLocation());
                           },
-                        );
-                      });
-            } else if (state is LocationSearchingState) {
-              return const SizedBox(
-                  key: Key('searching_indicator'),
-                  height: 200,
-                  width: double.infinity,
-                  child: Center(
-                      child: CircularProgressIndicator(
-                    color: Colors.white,
-                  )));
-            } else {
-              return Container();
-            }
-          })
-        ];
-      },
-    );
+                          icon: const Icon(
+                            Icons.my_location,
+                            color: Colors.white,
+                          ),
+                        )
+                ],
+              );
+            },
+          );
+        },
+        suggestionsBuilder:
+            (BuildContext context, SearchController controller) {
+          return [
+            BlocBuilder<LocationBloc, LocationState>(builder: (context, state) {
+              if (state is LocationSearchSuccessState) {
+                var locations = state.locations;
+                return locations.isEmpty
+                    ? const SizedBox(
+                        height: 200, child: Icon(Icons.hourglass_empty))
+                    : ListView.builder(
+                        key: const Key('location_data'),
+                        shrinkWrap: true,
+                        itemCount: locations.length,
+                        itemBuilder: (context, index) {
+                          final LocationEntity location = locations[index];
+                          return ListTile(
+                            leading: const Icon(Icons.location_pin),
+                            title: Text(location.name!),
+                            subtitle:
+                                Text("${location.state}, ${location.country}"),
+                            onTap: () {
+                              controller.closeView(location.name!);
+                              FocusScope.of(context).unfocus();
+                              onGetCurrentLocation(
+                                  location.lat!, location.lon!);
+                            },
+                          );
+                        });
+              } else if (state is LocationSearchingState) {
+                return const SizedBox(
+                    key: Key('searching_indicator'),
+                    height: 200,
+                    width: double.infinity,
+                    child: Center(
+                        child: CircularProgressIndicator(
+                      color: Colors.white,
+                    )));
+              } else {
+                return Container();
+              }
+            })
+          ];
+        },
+      );
+    });
   }
 
   void searchListener() {
